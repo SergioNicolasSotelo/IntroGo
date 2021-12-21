374e12225753ea623e0070206677a9ee9158486c
SergioNicolasSotelo <sergio.sotelo@mercadolibre.com>
2021-12-21 11:06:32 -0300

    avanzando tarea FMT

--- FMT_OS_IO/TT/TT.go
+++ FMT_OS_IO/TT/TT.go
@@ -24,3 +24,27 @@ func (u *usuario) cambiarCorreo(nuevoCorreo string) {
 func (u *usuario) cambiarContrasena(nuevoContrasena string) {
 	u.Contrasena = nuevoContrasena
 }
+
+type user struct {
+	Nombre    string
+	Apellido  string
+	Correo    string
+	Productos []Product
+}
+type Product struct {
+	Nombre   string
+	Precio   float64
+	Cantidad int
+}
+
+func nuevoProducto(nombre string, precio float64) Product {
+	nuevoProducto := Product{Nombre: nombre, Precio: precio}
+	return nuevoProducto
+}
+func agregarProducto(usuario user, producto *Product, cantidad int) {
+	producto.Cantidad = cantidad
+	usuario.Productos = append(usuario.Productos, *producto)
+}
+func borrarProductos(usuario user) {
+	usuario.Productos = nil
+}
