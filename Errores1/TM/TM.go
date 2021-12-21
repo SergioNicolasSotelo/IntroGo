e3166545e8846fe32b6a6f49cbaaf6ec0ae6e720
SergioNicolasSotelo <sergio.sotelo@mercadolibre.com>
2021-12-21 14:49:45 -0300

    agregar codigo a ejercicios.

--- Errores1/TM/TM.go
+++ Errores1/TM/TM.go
@@ -54,13 +54,18 @@ func impuestos3(salary int) (string, error) {
  Ejercicio 3 - Impuestos de salario #3
 Repite el proceso anterior, pero ahora implementando “fmt.Errorf()”, para que el mensaje de error reciba por parámetro el valor de “salary” indicando que no alcanza el mínimo imponible (el mensaje mostrado por consola deberá decir: “error: el mínimo imponible es de 150.000 y el salario ingresado es de: [salary]”, siendo [salary] el valor de tipo int pasado por parámetro).
 */
-func calcularSalario(horasMes int, valorHora float64) (string, error) {
-	salary := float64(horasMes) * valorHora
-	if salary < 150000 {
-		return "", fmt.Errorf("El salario ingresado, %f, no alcanza el mínimo imponible, que es de 150000.", salary)
+func calcularSalario(horasMes int, valorHora float64) (float64, error) {
+	var salary float64
+	if horasMes < 80 {
+		return 0, fmt.Errorf("error: el trabajador no puede haber trabajado menos de 80 hs mensuales")
 	} else {
-		return "Debe pagar impuesto", nil
+		salary = float64(horasMes) * valorHora
 	}
+
+	if salary >= 150000 {
+		return salary - (salary / 100 * 10), nil
+	}
+	return salary, nil
 }
 
 /*
@@ -70,4 +75,13 @@ Desarrolla las funciones necesarias para permitir a la empresa calcular:
 Salario mensual de un trabajador según la cantidad de horas trabajadas.
 La función recibirá las horas trabajadas en el mes y el valor de la hora como argumento.
 Dicha función deberá retornar más de un valor (salario calculado y error).
+En caso de que el salario mensual sea igual o superior a $150.000, se le deberá descontar el 10% en concepto de impuesto.
+En caso de que la cantidad de horas mensuales ingresadas sea menor a 80 o un número negativo, la función debe devolver un error. El mismo deberá indicar “error: el trabajador no puede haber trabajado menos de 80 hs mensuales”.
+Calcular el medio aguinaldo correspondiente al trabajador
+Fórmula de cálculo de aguinaldo:
+[mejor salario del semestre] / 12 * [meses trabajados en el semestre].
+La función que realice el cálculo deberá retornar más de un valor, incluyendo un error en caso de que se ingrese un número negativo.
+
+Desarrolla el código necesario para cumplir con las funcionalidades requeridas, utilizando “errors.New()”, “fmt.Errorf()” y “errors.Unwrap()”. No olvides realizar las validaciones de los retornos de error en tu función “main()”.
+
 */
