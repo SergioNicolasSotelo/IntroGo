49740b61bc74907ed52766b743fe67f02137403c
SergioNicolasSotelo <sergio.sotelo@mercadolibre.com>
2021-12-20 21:21:44 -0300

    tarea punteros avanzando.

--- FMT_OS_IO/TT/TT.go
+++ FMT_OS_IO/TT/TT.go
@@ -3,3 +3,24 @@ package main
 func main() {
 
 }
+
+type usuario struct {
+	Nombre     string
+	Apellido   string
+	Edad       int
+	Correo     string
+	Contrasena string
+}
+
+func (u *usuario) cambiarNombre(nuevoNombre string) {
+	u.Nombre = nuevoNombre
+}
+func (u *usuario) cambiarApellido(nuevoApellido string) {
+	u.Apellido = nuevoApellido
+}
+func (u *usuario) cambiarCorreo(nuevoCorreo string) {
+	u.Correo = nuevoCorreo
+}
+func (u *usuario) cambiarContrasena(nuevoContrasena string) {
+	u.Contrasena = nuevoContrasena
+}
